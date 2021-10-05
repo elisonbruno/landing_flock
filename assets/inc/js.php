3aa64b7cb3f0c2c8166f5710aecb6be8cda9c300
Elison Bruno <elisonbrunosilva@gmail.com>
2021-10-05 05:43:49 -0300

    Delete files

--- assets/inc/js.php
+++
@@ -1,8 +0,0 @@
-<script src="assets/jquery/dist/jquery.min.js"></script>
-	<script src="assets/popper.js/dist/umd/popper.min.js"></script>
-	<script src="assets/bootstrap/dist/js/bootstrap.min.js"></script>
-	<script src="assets/vendors/owl-carousel/owl.carousel.min.js"></script>
-	<script src="assets/vendors/superslides/jquery.superslides.min.js"></script>
-	<script src="https://unpkg.com/aos@next/dist/aos.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/js-cookie/2.2.0/js.cookie.min.js"></script>
-	<script src="assets/js/main.js"></script>
